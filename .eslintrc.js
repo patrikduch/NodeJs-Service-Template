16ea71f687194861b7ce78446ea9c0ffbc22bb29
patrikduch <duchpatrik@icloud.com>
2019-08-29 07:44:00 +0200

    ESLint rules update

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
 		'@typescript-eslint/no-unused-vars': 'off', // Vypnout striktni vynuceni pouziti importovane promenne
 		'@typescript-eslint/explicit-function-return-type': 'off', // Pro tsx nevraci typ
 		'@typescript-eslint/no-empty-interface': 'error', // Rozhrani nemohou zůstat prázdná (nepřenáší se to do ts souborů)
-		'@typescript-eslint/no-explicit-any': 'off' // Vynucené přiřazení any typu je zákazané
+		'@typescript-eslint/no-explicit-any': 'off', // Vynucené přiřazení any typu je zákazané
+		'@typescript-eslint/camelcase': 'off'
 	}
 };
